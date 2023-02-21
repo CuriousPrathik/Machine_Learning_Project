5288be5b92cb142e816f51f7e9564155cbda1ff8
Prathik Vishavadiya <108474251+CuriousPrathik@users.noreply.github.com>
2023-02-21 06:38:46 +0530

    logger & exception added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 # Create Flask obect
 app = Flask(__name__)
@@ -6,6 +9,12 @@ app = Flask(__name__)
 # Create a decorator function
 @app.route('/')
 def home_page():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
     return "Welcome to the Home Page"
 
 # run the app
